8551715f55d1f9ca3c7ec57f03159ac2520d083f
libinmoncy615 <libinmoncy615@gmail.com>
2024-06-16 13:20:12 +0530

    step-4

---
+++ Assignment_2_WebPage/script.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+function toggleTheme() {
+    const body = document.body;
+    if (body.getAttribute('data-theme') === 'dark') {
+        body.setAttribute('data-theme', 'light');
+    } else {
+        body.setAttribute('data-theme', 'dark');
+    }
+}
+
